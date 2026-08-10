05f35ca26d5f7283ce95751cb608cd049bd62201
kenanevrenkaba <knnevrn@gmail.com>
2026-08-11 02:42:02 +0300

    Şifre üreteci ana hatlarıyla oluşturuldu. birsonraki adımda Hata Kontrolü eklenecek.

--- SifreUreteci/Program.cs
+++ SifreUreteci/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hoş Geldiniz");
+
+string yinele = "";
+    
+while (yinele != "N")
+{
+    Console.Write("Kaç karakterden oluşan bir şifre belirlemek istiyorsunuz? ");
+        
+    string giris = (Console.ReadLine());
+
+    while (int.TryParse(giris, out int sifreUzunlugu) && yinele != "N")
+    {
+        string karakterHavuzu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+        string uretilenSifre = "";
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < sifreUzunlugu; i++)
+        {
+            int randomIndex = rnd.Next(0, karakterHavuzu.Length);
+
+            uretilenSifre += karakterHavuzu[randomIndex];
+        }
+
+        Console.WriteLine("Üretilen şifreniz: " + uretilenSifre);
+        Console.Write("Başka şifre üretmek istiyor musunuz? Y/N: ");
+        string cevap = Console.ReadLine();
+        yinele = cevap.ToUpper();
+    }
+    
+    Console.WriteLine("Lütfen sayısal bir değer giriniz!");
+}
+
+Console.WriteLine("Güle Güle...");
+
