ab3a7f053d463812c70aa4a9f0cddbcb633e748a
KENAN EVREN KABA <knnevrn@gmail.com>
2026-08-11 22:57:11 +0300

    Kullanıcı seçimleri ve bunların hata kontrolleri eklendi.

--- SifreUreteci/Program.cs
+++ SifreUreteci/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Hoş Geldiniz");
 
 string yinele = "";
-    
-while (yinele != "N")
+
+while (yinele != "H")
 {
     Console.Write("Kaç karakterden oluşan bir şifre belirlemek istiyorsunuz? ");
 
@@ -14,7 +14,52 @@ while (yinele != "N")
         Console.Write("Kaç karakterden oluşan bir şifre belirlemek istiyorsunuz? ");
     }
 
-    string karakterHavuzu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    string karakterHavuzu = "0123456789";
+
+    Console.Write("Şifreniz 'Büyük Harf' içersin mi? E/H: ");
+    string buyukHarf = Console.ReadLine().ToUpper();
+
+    while (buyukHarf != "E" && buyukHarf != "H")
+    {
+        Console.WriteLine("Lütfen geçerli bir seçim yapınız.");
+        Console.Write("Şifreniz 'Büyük Harf' içersin mi? E/H: ");
+        buyukHarf = Console.ReadLine().ToUpper();
+    }
+
+    if (buyukHarf == "E")
+    {
+        karakterHavuzu += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    Console.Write("Şifreniz 'Küçük Harf' içersin mi? E/H: ");
+    string kucukHarf = Console.ReadLine().ToUpper();
+
+    while (kucukHarf != "E" && kucukHarf != "H")
+    {
+        Console.WriteLine("Lütfen geçerli bir seçim yapınız.");
+        Console.Write("Şifreniz 'Küçük Harf' içersin mi? E/H: ");
+        kucukHarf = Console.ReadLine().ToUpper();
+    }
+
+    if (kucukHarf == "E")
+    {
+        karakterHavuzu += "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    Console.Write("Şifreniz 'Sembol' içersin mi? E/H: ");
+    string sembol = Console.ReadLine().ToUpper();
+
+    while (sembol != "E" && sembol != "H")
+    {
+        Console.WriteLine("Lütfen geçerli bir seçim yapınız.");
+        Console.Write("Şifreniz 'Sembol' içersin mi? E/H: ");
+        sembol = Console.ReadLine().ToUpper();
+    }
+
+    if (sembol == "E")
+    {
+        karakterHavuzu += "!@#$%^&*()";
+    }
 
     string uretilenSifre = "";
 
@@ -27,12 +72,40 @@ while (yinele != "N")
         uretilenSifre += karakterHavuzu[randomIndex];
     }
 
-    Console.WriteLine("Üretilen şifreniz: " + uretilenSifre);
-    Console.Write("Başka şifre üretmek istiyor musunuz? Y/N: ");
-    string cevap = Console.ReadLine();
-    yinele = cevap.ToUpper();
-    
+    Console.Write("Üretilen şifreniz: " + uretilenSifre);
+
+    if (sifreUzunlugu > 16 && buyukHarf == "E" && kucukHarf == "E" && sembol == "E")
+    {
+        Console.WriteLine(" -- Çok Güçlü Şifre");
+    }
+
+    else if ((sifreUzunlugu > 12 && buyukHarf == "E" && kucukHarf == "E" && sembol == "E") || sifreUzunlugu > 16)
+    {
+        Console.WriteLine(" -- Güçlü Şifre");
+    }
+
+    else if (sifreUzunlugu > 8)
+    {
+        Console.WriteLine(" -- Orta Şifre");
+    }
+
+    else
+    {
+        Console.WriteLine(" -- Zayıf Şifre");
+    }
+
+
+    Console.Write("Başka şifre üretmek istiyor musunuz? E/H: ");
+    string cevap = Console.ReadLine().ToUpper();
+
+    while (cevap != "E" && cevap != "H")
+    {
+        Console.WriteLine("Lütfen geçerli bir seçim yapınız.");
+        Console.Write("Başka şifre üretmek istiyor musunuz? E/H: ");
+        cevap = Console.ReadLine().ToUpper();
+    }
+
+    yinele = cevap;
 }
 
 Console.WriteLine("Güle Güle...");
-
