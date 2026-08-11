a0d482b085fcc3665e1a4bd3ba897b04967f2193
KENAN EVREN KABA <knnevrn@gmail.com>
2026-08-11 04:02:04 +0300

    Şifre Üreteci tamamlandı. Hata kontrolü eklendi.

--- SifreUreteci/Program.cs
+++ SifreUreteci/Program.cs
@@ -5,31 +5,33 @@ string yinele = "";
 while (yinele != "N")
 {
     Console.Write("Kaç karakterden oluşan bir şifre belirlemek istiyorsunuz? ");
-        
-    string giris = (Console.ReadLine());
 
-    while (int.TryParse(giris, out int sifreUzunlugu) && yinele != "N")
+    int sifreUzunlugu;
+
+    while (!int.TryParse(Console.ReadLine(), out sifreUzunlugu) || sifreUzunlugu <= 0)
     {
-        string karakterHavuzu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-
-        string uretilenSifre = "";
-
-        Random rnd = new Random();
-
-        for (int i = 0; i < sifreUzunlugu; i++)
-        {
-            int randomIndex = rnd.Next(0, karakterHavuzu.Length);
-
-            uretilenSifre += karakterHavuzu[randomIndex];
-        }
-
-        Console.WriteLine("Üretilen şifreniz: " + uretilenSifre);
-        Console.Write("Başka şifre üretmek istiyor musunuz? Y/N: ");
-        string cevap = Console.ReadLine();
-        yinele = cevap.ToUpper();
+        Console.WriteLine("Lütfen sayısal bir değer giriniz!");
+        Console.Write("Kaç karakterden oluşan bir şifre belirlemek istiyorsunuz? ");
     }
+
+    string karakterHavuzu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+    string uretilenSifre = "";
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < sifreUzunlugu; i++)
+    {
+        int randomIndex = rnd.Next(0, karakterHavuzu.Length);
+
+        uretilenSifre += karakterHavuzu[randomIndex];
+    }
+
+    Console.WriteLine("Üretilen şifreniz: " + uretilenSifre);
+    Console.Write("Başka şifre üretmek istiyor musunuz? Y/N: ");
+    string cevap = Console.ReadLine();
+    yinele = cevap.ToUpper();
     
-    Console.WriteLine("Lütfen sayısal bir değer giriniz!");
 }
 
 Console.WriteLine("Güle Güle...");
